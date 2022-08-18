d467afa7b52bbb1a503a29e0c1e66993e8e770d3
gregreust <greg.reust@gmail.com>
2022-08-18 14:00:13 -0500

    Created products model and migrated

--- products/models.py
+++ products/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Products(models.Model):
+    title = models.CharField(max_length=200)                        
+    description = models.CharField(max_length=200)
+    price = models.DecimalField(max_digits=9, decimal_places=2)     
+    inventory_quantity = models.IntegerField()
+    image_link = models.CharField(max_length=300)
